04d3cf309ac65b13fbbd6d94bba942dc71c3b114
haruboting <raiu_hk@icloud.com>
2023-03-18 00:11:41 +0900

    setting

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def get_dataset(tokenizer, type_path, args):
 class T5FineTuner(pl.LightningModule):
     def __init__(self, hparams):
         super(T5FineTuner, self).__init__()
-        self.hparams = hparams
+        self.hparams.update(vars(hparams))
 
         self.model = T5ForConditionalGeneration.from_pretrained(hparams.model_name_or_path)
         self.tokenizer = T5Tokenizer.from_pretrained(hparams.model_name_or_path)
